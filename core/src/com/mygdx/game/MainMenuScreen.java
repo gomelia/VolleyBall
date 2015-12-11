b6febeb74a4ca2c40f3ded09cba5d22eeb8af79f
s0912282 <s0912282@gmail.com>
2015-12-11 15:54:32 -0500

    Main Menu button

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -1,26 +1,99 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainMenuScreen implements Screen {
 
-    final VolleyBall game;
+    VolleyBall game;
 
+    Skin skin;
+    Stage stage;
+    SpriteBatch batch;
+    Viewport vport;
+
+    Game g;
     OrthographicCamera camera;
 
     public MainMenuScreen(final VolleyBall game) {
+        create();
         this.game = game;
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
+        //camera = new OrthographicCamera();
+        //camera.setToOrtho(false, 800, 480);
+    }
+    MainMenuScreen()
+    {
+        create();
+    }
+
+    public void create() {
+        batch = new SpriteBatch();
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+        // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+        skin = new Skin();
+        // Generate a 1x1 white texture and store it in the skin named "white".
+        Pixmap pixmap = new Pixmap(100, 100, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.GREEN);
+        pixmap.fill();
+
+        skin.add("white", new Texture(pixmap));
+
+        // Store the default libgdx font under the name "default".
+        BitmapFont bfont = new BitmapFont();
+        bfont.getData().setScale(1, 1);
+        skin.add("default", bfont);
+
+        // Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+
+        textButtonStyle.font = skin.getFont("default");
+
+        skin.add("default", textButtonStyle);
+
+        // Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+        final TextButton textButton = new TextButton("PLAY", textButtonStyle);
+        textButton.setPosition(200, 200);
+        stage.addActor(textButton);
+        //stage.addActor(textButton);
+        //stage.addActor(textButton);
+        textButton.addListener(new ChangeListener() {
+                                   public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+                                       //System.out.println("Clicked! Is checked: " + button.isChecked());
+                                       textButton.setText("Starting new game");
+                                       game.setScreen(new GameScreen(game));
+                                       dispose();
+
+                                   }
+
+                               }
+        );
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+
+
+       /* Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
@@ -35,10 +108,27 @@ public class MainMenuScreen implements Screen {
             game.setScreen(new GameScreen(game));
             dispose();
         }
+        */
+
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        //Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+       // Table.drawDebug(stage);
     }
 
     @Override
-    public void resize(int width, int height) {}
+    public void resize (int width, int height) {
+        System.out.println(width +" " + height);
+        //vport.setScreenSize(width, height);
+        //stage.setViewport(vport);
+    }
+
+    @Override
+    public void dispose () {
+        stage.dispose();
+        skin.dispose();
+    }
     @Override
     public void show() {}
     @Override
@@ -47,6 +137,4 @@ public class MainMenuScreen implements Screen {
     public void pause() {}
     @Override
     public void resume() {}
-    @Override
-    public void dispose() {}
 }
