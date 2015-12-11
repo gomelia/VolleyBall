33b25b2b5d4cf8f68293ef387f9c16075e9f2a79
c-rebelo <s0947449@monmouth.edu>
2015-12-11 16:05:54 -0500

    main screen picture

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -5,12 +5,17 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainMenuScreen implements Screen {
 
     final VolleyBall game;
     GameScreen gameScreen = null;
     Sound gameMusic;
+    Texture background;
+    Sprite backgroundSprite;
 
     OrthographicCamera camera;
 
@@ -20,20 +25,26 @@ public class MainMenuScreen implements Screen {
         this.game = game;
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
+        camera.setToOrtho(false, 1200, 900);
         gameMusic = Gdx.audio.newSound(Gdx.files.internal("Sumo_music.mp3"));
         gameMusic.play();
+        background = new Texture("mainscreen.jpg");
+        backgroundSprite = new Sprite(background);
+        backgroundSprite.setBounds(0f, 0f, 1200f, 900f);
+        //backgroundSprite.setCenter(0, 0);
+
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+       // Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
+        backgroundSprite.draw(game.batch);
         game.font.draw(game.batch, "Welcome to Hippo VolleyBall!!! ", 100, 150);
         game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
         game.batch.end();
